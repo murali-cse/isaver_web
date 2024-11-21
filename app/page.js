97de0bbf30f03d00568a1e73fd2ff3f788da6abe
murali-cse <muralicse97@gmail.com>
2024-11-22 00:06:55 +0530

    bug fixed apk updated

--- app/page.js
+++ app/page.js
@@ -27,7 +27,7 @@ export default function Home() {
           </p>
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="./v1.2.0.apk"
+            href="./isaver.apk"
             target="_blank"
             rel="noopener noreferrer"
             download={true}
